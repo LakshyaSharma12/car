201283a46b4017b98ef8a1c8784384c7c1a7d00d
LakshyaSharma12 <71214689+LakshyaSharma12@users.noreply.github.com>
2021-02-12 16:50:21 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var gamestate=0
+var playerCount
+var position;
+var allPlayers
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+
+  
+
+
+
+}
+
+function draw(){
+  background("white");
+  
+   
+  
+}
+
+
